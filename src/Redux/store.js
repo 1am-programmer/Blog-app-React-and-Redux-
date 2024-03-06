456e802e5becb7304cf14ab02eee7de21ca31e09
Daniel <155446371+1am-programmer@users.noreply.github.com>
2024-03-06 02:47:37 -0800

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./post";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, postReducer);
 
 export const store = configureStore({
   reducer: {
-    posts: postReducer,
+    posts: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
+
